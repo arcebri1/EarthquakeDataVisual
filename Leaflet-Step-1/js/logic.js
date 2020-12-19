0ff394a3f9d86d2e6062259f7bf461bd2458a054
Lorena Arce <you@example.com>
2020-12-19 01:26:02 -0800

    read through the geojson, created variable for features, start the loop

--- Leaflet-Step-1/js/logic.js
+++ Leaflet-Step-1/js/logic.js
@@ -1,7 +1,7 @@
 //Check the array of the data
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function(data) {
-    console.log(data)
-})
+// d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function(data) {
+//     console.log(data)
+// })
 
 //Create a map object, I chose the middle of US as the center
 let myMap = L.map("map", {
@@ -15,7 +15,23 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
-    id: "mapbox/streets-v11",
+    id: "mapbox/light-v10",
     accessToken: API_KEY
   }).addTo(myMap);
 
+url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+
+//Read through the data
+d3.json(url, function(data) {
+    console.log(data)
+
+//create a variable that calls up the array features witin the data in the geojson
+    let sites = data.features
+    console.log(sites)
+    
+//Loop through each object to call up the magnitude
+//and coordinates of each earthquake
+    for (let i=0; i < sites.length; i++) {
+
+    }
+})
