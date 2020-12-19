1dfe1de33bf8317f96b1fcfad2252355c3f904d4
Lorena Arce <you@example.com>
2020-12-19 01:02:48 -0800

    read data, created map object and base map layer

---
+++ Leaflet-Step-1/js/logic.js
@@ -0,0 +1,21 @@
+//Check the array of the data
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function(data) {
+    console.log(data)
+})
+
+//Create a map object, I chose the middle of US as the center
+let myMap = L.map("map", {
+    center: [39.50, -98.35],
+    zoom: 4
+})
+
+//Create the base map layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
